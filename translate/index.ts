abbc53b2b7b5c562f22dc0b0ceabac9dade6cbd3
Csoki <feketetibi71@gmail.com>
2024-05-01 20:24:45 +0200

    feat: add hungarian translations

--- translate/index.ts
+++ translate/index.ts
@@ -14,4 +14,16 @@ setBulk({
         'auth.span.new.user': 'New User?',
         'auth.span.existing.user': 'Existing User?',
     },
+    hu: {
+        'auth.span.login': 'Bejelentkezés',
+        'auth.span.register': 'Regisztráció',
+        'auth.span.remember': 'Jegyezz meg',
+        'auth.span.bad.password': 'Hibás email cím vagy jelszó',
+        'auth.span.register.error': 'Hiba történt, próbáld újra',
+        'auth.span.email': 'email',
+        'auth.span.password': 'jelszó',
+        'auth.span.password.again': 'jelszó ismét',
+        'auth.span.new.user': 'Új felhasználó?',
+        'auth.span.existing.user': 'Van már fiókod?',
+    },
 });
